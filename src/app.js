2365da937527e7b4ad0ab2067613c5e47f0882a6
hafiz07 <hkkhan074@gmail.com>
2026-07-04 19:57:51 +0600

    added newly

--- src/app.js
+++ src/app.js
@@ -399,7 +399,6 @@ app.get('/', (req, res) => {
 // API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-
 app.use('/api/ideas', ideaRoutes);
 app.use('/api/chats', chatRoutes);
 app.use('/api/messages', messageRoutes);
@@ -437,5 +436,3 @@ import { errorHandler } from './services/errorService.js';
 app.use(errorHandler);
 
 export default app; 
-
-
